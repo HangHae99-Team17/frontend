3e01662639389f7e7347f44f2201002e92b9ae2a
parksangsoo <pssw0728@gmail.com>
2021-10-29 19:13:40 +0900

    axios category 수정

--- src/common/axios.js
+++ src/common/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://13.209.12.105:8080/",
+  baseURL: "http://3.34.199.152/",
 });
 
 instance.interceptors.request.use((config) => {
@@ -14,18 +14,14 @@ instance.interceptors.request.use((config) => {
 export const apis = {
   // 할인 정보 리스트 가지고 오기
   getList: () => instance.get('/api/main'),
-  
-  // 게시물 불러오기
   adduser: (user_info) => instance.post('/api/user/signup',user_info),
   loginuser: (user_info) => instance.post('/api/user/login',user_info),
   logincheck: () => instance.get('api/user/show'),
-  
-  getCategory:() => instance.get('api/main'),
-  
-  getFolders:() => instance.get('api/folders'),
-
   edituser: (user_info) => instance.put('/api/user/change',user_info),
   deluser: (password) => instance.post('/api/user/delete',password),
+  getCategory:() => instance.get('api/main'),
+  getFolders:() => instance.get('api/folders'),
+  
   // 쿠폰 상세페이지 데이터 불러오기
   getDetail:(param)=>instance.get(`api/detail/${param}`),
 };
