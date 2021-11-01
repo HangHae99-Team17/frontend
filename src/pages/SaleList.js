21abfe6c46d9a42127e60a1a4347bd82b092c7d5
parksangsoo <pssw0728@gmail.com>
2021-11-01 20:10:18 +0900

    관리자 회원가입, 쿠폰입력,불러오기,삭제 기능 완료

---
+++ src/pages/SaleList.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { actionCreators as saleActions } from "../redux/modules/sale";
+import Coupon from '../components/Coupon';
+
+const SaleList = () => {
+
+    const dispatch = useDispatch();
+    const sale_list = useSelector((state) => state.sale.list);
+
+    useEffect(()=>{
+        dispatch(saleActions.setSaleFB());
+    },[])
+
+    return (
+        <React.Fragment>
+            <h3>쿠폰목록</h3>
+            {sale_list.map((sale) => {
+                return(
+                    <Coupon key={sale.id} {...sale}/>
+                )
+            })}
+        </React.Fragment>
+    );
+};
+
+export default SaleList;
