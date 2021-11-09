3f6f1da4fe4f3d6fb1604e9a4a06dc04ff94a474
parksangsoo <pssw0728@gmail.com>
2021-11-09 10:42:00 +0900

    회원가입 css 작업

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { actionCreators as userActions } from "../redux/modules/user";
 import styled from 'styled-components';
+import { history } from "../redux/configureStore";
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -36,18 +37,62 @@ const Login = () => {
     return (
         <React.Fragment>
             <LoginBox>
-                <p>이메일</p>
-                <input type="text" name="email" value={email} onChange={onChange}/>
-                <p>패스워드</p>
-                <input type="password" name="password" value={password} onChange={onChange}/>
-                <button onClick={submitLogin}>로그인</button>
+                <IdInputBox>
+                    <input type="text" placeholder="아이디(이메일)" name="email" value={email} onChange={onChange}/>
+                </IdInputBox>
+                <PasswordInputBox>
+                    <input type="password" placeholder="비밀번호" name="password" value={password} onChange={onChange}/>
+                </PasswordInputBox>
+                <LoginButton onClick={submitLogin}>로그인하기</LoginButton>
+                <SignupBox>
+                    <button onClick={()=>{
+                        window.location.replace('/signup');
+                    }}>회원가입</button>
+                </SignupBox>
             </LoginBox>
         </React.Fragment>
     );
 };
 
 const LoginBox = styled.div`
-    
+    text-align: center;
+
 `;
 
+const IdInputBox = styled.div`
+    margin-bottom: 10px;
+    margin-top: 30px;
+    input{
+        width: 328px;
+        height: 37px;
+        border:1px solid #D5D5D5;
+    }
+`;
+
+const PasswordInputBox = styled.div`
+    margin-bottom: 60px;
+    input{
+        width: 328px;
+        height: 37px;
+        border:1px solid #D5D5D5;
+    }
+`;
+
+const LoginButton = styled.button`
+    width: 328px;
+    height: 45px;
+    background-color: orange;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 15px;
+`;
+
+const SignupBox = styled.div`
+    button{
+        font-size: 13px;
+        border:none;
+        background-color: white;
+    }
+`;
 export default Login;
