1cf6a046383a8df91fcfcbe204f97d702f4c7172
parksangsoo <pssw0728@gmail.com>
2021-11-20 20:04:38 +0900

    ㄷㄷㄷ

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState,useCallback} from 'react';
+import React,{useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { actionCreators as userActions } from "../redux/modules/user";
 import styled from 'styled-components';
