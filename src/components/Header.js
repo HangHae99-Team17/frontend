963bb6a25ba30ba4333908231b12063081bda808
mingab91 <mingab91@naver.com>
2021-11-30 12:44:59 +0900

    디테일페이지 스타일 변경
    디테일페이지 스타일 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -232,7 +232,7 @@ const Header = (props) => {
             ):(
               <LoginButton onClick={() => {
                 history.push("/login");
-              }}>asdafdasfdsafdsafasfsa</LoginButton>
+              }}>로그인하기</LoginButton>
             )}
             </div>)}
             <div className="searchIcon">  
